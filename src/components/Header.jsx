6c4ac1b46e6fa4e07b15ee7a1e1ebbfbc56892fd
Lucky <luckykashyap3430@gmail.com>
2023-10-20 22:41:17 +0530

    use custom hook for api calling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container">
     <img  className='news-logo img-responsive' src={logo} alt="" />
-    <Link className="navbar-brand" href="#">News API</Link>
+    <Link className="navbar-brand" to="/">News API</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import NewsCard from '../components/NewsCard';
 import './Home.css';
+import useFetch from '../hooks/useFetch';
 
 const Home = () => {
-    const [news,setNews] = useState([]);
+    const {data : news} = useFetch();
 
-    useEffect(()=>{
-        fetchNews();
-    },[]);
-
-    const fetchNews=async()=>{
-        const data = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=98c9bb1fd0ea4e3391414166dfae5c1f');
-        const res = await data.json();
-        setNews(res.articles);
-    }
+    
   return (
     <div className='container'>
         <div className='box'>
